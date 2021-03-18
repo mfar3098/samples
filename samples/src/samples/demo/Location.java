f9b212649ac2ad957486740569fdb98a9c154572
Muhammad Firdaus bin Abdul Rahim <mfar3098@gmail.com>
2021-03-18 18:23:52 +0800

    Location Second Commit

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -4,6 +4,8 @@ public class Location {
 	private String name;
 	private int locationID;
 	private int description;
+	private int locationCity;
+	private int locationState;
 	
 	public String getName() {
 		return name;
